9f686daac1eaea08c022c79f5e50e2651780e781
magic8baller <39971956+magic8baller@users.noreply.github.com>
2018-07-18 11:28:32 -0400

    Delete incorrect file

--- CA-arrays2.js
+++
@@ -1,3 +0,0 @@
-FUNCTIONS
--block of JS that robotically does the same thing over & over-- WHENEVER YOU CALL ITS NAME
--seen as the JavaScript’s workhorses. They alone play roles that other languages fulfill with multiple distinct features: procedures, methods, constructors and even classes and modules. Every one of this is covered by the versatility of functions.
