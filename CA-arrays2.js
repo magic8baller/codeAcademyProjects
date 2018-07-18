799671ca6121e826f2c70909edc54df9ba8098bb
magic8baller <39971956+magic8baller@users.noreply.github.com>
2018-07-18 11:24:42 -0400

    Add files via upload

---
+++ CA-arrays2.js
@@ -0,0 +1,3 @@
+FUNCTIONS
+-block of JS that robotically does the same thing over & over-- WHENEVER YOU CALL ITS NAME
+-seen as the JavaScript’s workhorses. They alone play roles that other languages fulfill with multiple distinct features: procedures, methods, constructors and even classes and modules. Every one of this is covered by the versatility of functions.
